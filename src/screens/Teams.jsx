12cbcb7f023b0362bcd41f35fa5a8c7afebf9087
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-07 09:24:30 -0400

    removes unnecessary files

--- src/screens/Teams.jsx
+++ src/screens/Teams.jsx
@@ -1,13 +1,8 @@
-import { useState, useEffect } from 'react';
-import { getTeams, getTeamsEast, getTeamsWest } from '../services/teams.js';
-import Team from '../components/Team.jsx';
-import Conferences from '../components/Conferences.jsx';
 import Accordion from '../components/Accordion.jsx';
 
 export default function Teams() {
   return (
     <div>
-      <Conferences />
       <Accordion />
     </div>
   )
