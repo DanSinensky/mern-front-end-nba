8a2574fb6921f1b9caf709accbe27697e660d21a
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-08-07 11:05:00 -0400

    fixes delete rendering and redirect

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,38 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { getTeam, deleteTeam } from '../services/teams.js'
 
 
 export default function AccordionItem(team) {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
   const [team1, setTeam1] = useState({})
-  let { id } = useParams()
+  const [isDeleted, setDeleted] = useState(false)
   let navigate = useNavigate()
   useEffect(() => {
     fetchTeam()
   }, [])
   async function fetchTeam() {
-    const oneTeam = await getTeam(id)
+    const oneTeam = await getTeam(team._id)
     setTeam1(oneTeam)
   }
   async function handleDelete() {
-    await deleteTeam(id)
-    navigate("/teams")
+    setDeleted(true)
+    await deleteTeam(team._id)
   }
-  return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <div>{team.abbreviation}</div>
-        <div>{isActive ? '-' : '+'}</div>
+  if (isDeleted === true) {
+    return <div></div>
+  } else {
+    return (
+      <div className="accordion-item">
+        <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+          <div>{team.abbreviation}</div>
+          <div>{isActive ? '-' : '+'}</div>
+        </div>
+        {isActive && <div className="accordion-content">
+          <h2>{team.full_name}</h2>
+          <p>{team.conference} - {team.division} - {team.city}</p>
+          <Link to={`/teams/${team._id}/edit`}>
+            <button>Edit Team</button>
+          </Link>
+          <button onClick={handleDelete}>Delete Team</button>
+        </div>}
       </div>
-      {isActive && <div className="accordion-content">
-        <h2>{team.full_name}</h2>
-        <p>{team.conference} - {team.division} - {team.city}</p>
-        <Link to={`/teams/${team._id}/edit`}> 
-          <button>Edit Team</button>
-        </Link>
-        <button onClick={handleDelete}>Delete Team</button>
-      </div>}
-    </div>
-  )
+    )
+  }
 }
